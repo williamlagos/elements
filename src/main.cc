c39a28831676e64fa8e448da1bcbaf05a2dcb934
William Oliveira de Lagos <william.lagos@icloud.com>
2020-03-23 19:30:36 -0300

    Added a valid compiling configuration

--- src/main.cc
+++ src/main.cc
@@ -1,11 +1,11 @@
 #define _APP_MAIN_CPP_
 
-#include "aeroshell.h"
-#include <QApplication>
+/* #include "aeroshell.h"
+#include <QApplication> */
 
 int main(int argc, char *argv[])
 {
-    Toolbox_malloc  = (void *(__cdecl *)(size_t))malloc;
+    /* Toolbox_malloc  = (void *(__cdecl *)(size_t))malloc;
     Toolbox_calloc  = (void *(__cdecl *)(size_t,size_t))calloc;
     Toolbox_realloc = (void *(__cdecl *)(void*,size_t))realloc;
     Toolbox_free    = (void (__cdecl *)(void *))free;
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     AERoshell *poAERoshell = new AERoshell(&a);
     int nReturnCode = a.exec();
-    delete poAERoshell;
+    delete poAERoshell; */
 
-    return nReturnCode;
+    return 0;
 }
