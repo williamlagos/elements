005d5903f81eef3b92330b9c6e33c34ffacd5380
William Oliveira de Lagos <william.lagos@icloud.com>
2020-03-23 21:32:36 -0300

    Cleared some codes with dead ends

--- include/elements.h
+++ include/elements.h
@@ -17,7 +17,7 @@
  * along with elements.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <JSON/libjson.h>
+// #include <JSON/libjson.h>
 #include <fstream>
 
 void load_json(const char* name);
